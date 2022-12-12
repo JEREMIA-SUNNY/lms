acac2b70bc9d460a97686b81c1f25cd579070449
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2022-12-12 13:50:31 +0530

    responsive login page done

--- components/Auth/login.js
+++ components/Auth/login.js
@@ -18,7 +18,7 @@ const LoginForm = () => {
                 </div>
             </div>
             <div className="p-6 space-y-2 sm:p-8">
-                <form className="space-y-4 md:space-y-6" action="#">
+                <form className="space-y-4 md:space-y-6">
                     <div>
                         <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                         <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
