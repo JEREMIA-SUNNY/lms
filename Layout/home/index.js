bc747d4fffb1fc4505867b984458e0a3408869ae
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2022-12-14 14:44:18 +0530

    navbar bar iss fixed

--- Layout/home/index.js
+++ Layout/home/index.js
@@ -6,7 +6,7 @@ import Transaction from "./transaction";
 
 const HomeLayout = () => {
     return (
-        <section className="mt-8 h-[82vh] w-full grid grid-rows-2 grid-flow-col gap-4">
+        <section className="mt-2 h-[82vh] w-full grid grid-rows-2 grid-flow-col gap-4">
             <Card className='p-4 lg:w-1/2'><Notification /></Card>
             <Card className='p-4 lg:w-1/2'><Pending /></Card>
             <Card className='p-4 lg:w-1/2'><Recent /></Card>
