da740de375d2991f21d6ddbca00b3ef96f034e62
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2022-12-15 23:03:38 +0530

    go back button

--- Layout/learning/coursedetail.js
+++ Layout/learning/coursedetail.js
@@ -8,7 +8,7 @@ const CourseDetailLayout = () => {
     return (
         <>
             <div className="mt-4 flex items-center justify-between">
-                <GoBackButton />
+                <GoBackButton title={'Course Details'} />
                 <div className="flex ustify-start">
                     <button className="rounded border-0 inline-flex items-center justify-center text-gray-500 mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#419a00"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zm-8-2h2v-4h4v-2h-4V7h-2v4H7v2h4z" /></svg>
--- components/goBackBtn/index.js
+++ components/goBackBtn/index.js
@@ -1,11 +1,11 @@
 import Router from 'next/router'
 
-const GoBackButton = () => {
+const GoBackButton = ({ title }) => {
     return (
-        <button onClick={() => Router.back()} type="button" className="flex justify-start items-center py-2 px-3 text-xs font-medium text-center text-[#419a00] rounded">
+        <button onClick={() => Router.back()} type="button" className="flex justify-start items-center py-2 font-medium text-[#419a00]">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#419a00"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21v-2z" /></svg>
             <span className="ml-1">
-                Extra small
+                {title}
             </span>
         </button>
     )
