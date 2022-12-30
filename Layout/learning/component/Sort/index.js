4e4ebb6d9b1e48b60097ff30826244cf9ab7eeed
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2022-12-30 22:10:51 +0530

    added sort option in landing page

---
+++ Layout/learning/component/Sort/index.js
@@ -0,0 +1,25 @@
+
+const list = [
+    { name: 'Avg Rating', title: 'Avg Rating' },
+    { name: 'Price(H-L)', title: 'Price (High-Low)' },
+    { name: 'Price(L-H)', title: 'Price (Low-High)' },
+    { name: 'Popularity', title: 'Popularity' },
+    { name: 'Recent', title: 'Most Recent' },
+]
+
+const Sortby = () => {
+    return (
+        <div className="w-28 ml-2">
+            <select name='sortby' className="outline-gray-200 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5">
+                <option>Sort by </option>
+                {list?.map((item, i) => {
+                    return (
+                        <option key={i} value={item?.value}>{item?.title}</option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default Sortby;
--- Layout/learning/course/detailHeader.js
+++ Layout/learning/course/detailHeader.js
@@ -33,7 +33,8 @@ const DetailHeader = ({ id }) => {
                     </tr> */}
                 </tbody>
             </table>
-            <p className="mt-2 text-gray-900">
+            <p className="font-semibld">Description: </p>
+            <p className="mt-2 text-gray-700">
                 n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final co
             </p>
         </div>
