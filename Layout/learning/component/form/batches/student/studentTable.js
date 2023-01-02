7e38f777e9aaff63c6ec4077250b88d9705169c0
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-01-02 23:48:45 +0530

    added approve ui in batch section

---
+++ Layout/learning/component/form/batches/student/studentTable.js
@@ -0,0 +1,14 @@
+import Table from './Table'
+import StudentJson from '../../../../../../assets/mokedata/student.json';
+
+const StudentTable = () => {
+
+    return (
+        <div className="w-full overflow-scroll h-full">
+            <Table data={StudentJson} />
+        </div>
+
+    )
+}
+
+export default StudentTable;
