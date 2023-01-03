b13ef1c2e6b20027b2c697effbbb938ce29a842e
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-01-03 18:30:56 +0530

    working on edit training and course

---
+++ Layout/learning/component/form/module/edit/assessment/index.js
@@ -0,0 +1,48 @@
+import AddAssessment from "./AddAssessment"
+import { Tab } from '@headlessui/react'
+import ModuleTabsList from "../moduletabslist"
+import PrimaryButton from '@/components/custom/Buttons/PrimaryButton';
+
+const MainAssessment = (props) => {
+    const { trainingInfo,
+        handleTrainingInput,
+        listofSection,
+        setListOfSection,
+        createQuestionForm,
+        handleAllQuestionInfo, listofModule } = props
+
+    return (
+        <div className="w-full">
+            <Tab.Group>
+                <ModuleTabsList listofModule={listofModule} />
+                <Tab.Panels className='h-[90%]'>
+                    {
+                        listofModule.map((module, i) => {
+                            return (
+                                <Tab.Panel key={i} className='h-full'>
+                                    <AddAssessment
+                                        module={module}
+                                        trainingInfo={trainingInfo}
+                                        handleInput={handleTrainingInput}
+                                        listofSection={listofSection}
+                                        setListOfSection={setListOfSection}
+                                        createQuestionForm={createQuestionForm}
+                                        handleAllQuestionInfo={handleAllQuestionInfo}
+                                    />
+                                </Tab.Panel>
+                            )
+                        })
+                    }
+                </Tab.Panels>
+            </Tab.Group>
+            <div className='flex justify-end fixed bottom-0 right-5'>
+                <PrimaryButton>
+                    <span>Add Assessment</span>
+                </PrimaryButton>
+            </div>
+
+        </div>
+    )
+}
+
+export default MainAssessment
---
+++ Layout/learning/component/form/module/edit/mainModuleEdit.js
@@ -0,0 +1,36 @@
+import ModuleTabsList from './moduletabslist'
+import { Tab } from '@headlessui/react'
+import ModuleForm from './moduleForm'
+import PrimaryButton from '@/components/custom/Buttons/PrimaryButton';
+
+const MainModuleCreate = ({ listofModule, IsAssessment, handleModuleName, ModuleFieldValue, moduleVideoInput }) => {
+
+    return (
+        <div className='w-full'>
+            <Tab.Group>
+                <ModuleTabsList listofModule={listofModule} />
+                <Tab.Panels className='h-[90%]'>
+                    {
+                        listofModule.map((item, i) => {
+                            return (
+                                <Tab.Panel key={i} className='h-full'>
+                                    <ModuleForm handleModuleName={handleModuleName}
+                                        IsAssessment={IsAssessment}
+                                        moduleVideoInput={moduleVideoInput}
+                                        ModuleFieldValue={ModuleFieldValue} item={item} index={i} />
+                                </Tab.Panel>
+                            )
+                        })
+                    }
+                </Tab.Panels>
+            </Tab.Group>
+            <div className='flex justify-end fixed bottom-0 right-5'>
+                <PrimaryButton>
+                    <span>Add Module</span>
+                </PrimaryButton>
+            </div>
+        </div>
+    )
+}
+
+export default MainModuleCreate
