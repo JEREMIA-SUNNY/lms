77d69b9e4858dfb184e644dd892ed87a9ab81113
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-01-11 10:39:05 +0530

    Change ui of approve button in batches and rename to approvals

--- Layout/learning/plan/Plandetail.js
+++ Layout/learning/plan/Plandetail.js
@@ -5,29 +5,26 @@ import CourseTrainingCard from "./courseTrainingCard";
 
 const PlanDetailLayout = () => {
     const route = useRouter()
-    const { query } = route
+    // const { query } = route
 
     return (
         <>
             <PlanAction />
             <section className="my-4 w-full bg-white p-4 shadow rounded-lg border border-[#419a00]">
                 <div className="bg-gray-100 p-2 rounded-lg">
+                    <div>
+                        <p><span className="font-semibld">Plan Name: </span> <span className="text-gray-700"> here is anme</span></p>
+                    </div>
+                    <div>
+                        <p><span className="font-semibld">Assigned to: </span> <span className="text-gray-700">Marketing Department</span></p>
+                    </div>
                     <div className="grid grid-cols-4 gap-4">
-                        <div>
-                            <p><span className="font-semibld">Plan Name: </span> <span> here is anme</span></p>
-                        </div>
-                        <div>
-                            <p><span className="font-semibld">Courses: </span> 23<span></span></p>
-                        </div>
-                        <div>
-                            <p><span className="font-semibld">Trainings: </span>23 <span></span></p>
-                        </div>
-                        <div>
-                            <p><span className="font-semibld">Assigned to: </span> Marketing Department <span></span></p>
-                        </div>
+
                     </div>
                     <p className="font-semibld">Description: </p>
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. L <span></span></p>
+                    <p className="text-gray-700">
+                        n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final co
+                    </p>
                 </div>
 
 
--- components/custom/Buttons/PrimaryButton.js
+++ components/custom/Buttons/PrimaryButton.js
@@ -1,8 +1,8 @@
 
 
-const PrimaryButton = ({ children }) => {
+const PrimaryButton = ({ children, onClick }) => {
     return (
-        <button type="button" className="text-white bg-green-600 hover:bg-green-700 focus:ring-0 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
+        <button onClick={onClick} type="button" className="text-white bg-green-600 hover:bg-green-700 focus:ring-0 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
             {children}
         </button>
     )
