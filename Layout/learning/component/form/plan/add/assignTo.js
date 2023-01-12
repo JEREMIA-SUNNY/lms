b34048e549611ff1affbca04541fb4b838ab18de
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-01-12 22:51:12 +0530

    stepper in add course and training ui card

--- Layout/learning/component/form/plan/add/assignTo.js
+++ Layout/learning/component/form/plan/add/assignTo.js
@@ -68,7 +68,6 @@ const AssignTo = ({ handleInput, planPayload }) => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                             </svg> : ''}
                         </button>
-                        {console.log(selectedUserList?.length)}
                         <span className='ml-2'>
                             Select
                         </span>
--- Layout/learning/plan/Plandetail.js
+++ Layout/learning/plan/Plandetail.js
@@ -23,7 +23,7 @@ const PlanDetailLayout = () => {
                         </div>
                     </div>
                     <div className="font-semibld">
-                        Description: <span span className="text-gray-700">
+                        Description: <span className="text-gray-700">
                             n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final co
                         </span>
                     </div>
