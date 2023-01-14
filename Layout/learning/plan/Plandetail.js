3908fae9a6f4c017bc1790f8c54730cbc4a1a983
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-01-14 21:51:05 +0530

    fixed ui of plan learning jeader

--- Layout/learning/plan/Plandetail.js
+++ Layout/learning/plan/Plandetail.js
@@ -13,20 +13,26 @@ const PlanDetailLayout = () => {
             <PlanAction />
             <section className="my-4 w-full bg-white p-4 shadow rounded-lg border border-[#419a00]">
                 <div className="bg-gray-100 shadow p-2 rounded-lg font-semibld">
-                    <div className="grid grid-cols-2 ">
-                        <div className="font-semibld">Plan Name: <span className="text-gray-700"> here is anme</span></div>
-                        <div className="flex justify-start items-center">
-                            <div className="font-semibld">Assigned to: <span className="text-gray-700 h-8">Marketing Department {'>'} </span></div> <div > <PlanInfoViewUsers /> </div>
+                    <div className="grid grid-cols-4">
+                        <div className='flex'>
+                            <div className="w-1/2 font-semibld">Plan Name <span className="text-gray-700"> : here is anme</span></div>
                         </div>
-                        <div className="font-semibld">Mandatory: <span className="text-gray-700">Yes</span></div>
-                        <div className="font-semibld">Due date: <span className="text-gray-700">10/02/2023</span></div>
-                        <div className="font-semibld">Status: <span className="text-gray-700"> Completed</span></div>
-
+                        <div className='flex'>
+                            <div className="w-1/2 font-semibld">Mandatory <span className="text-gray-700">:Yes</span></div>
+                        </div>
+                        <div className='flex'>
+                            <div className="w-1/2 font-semibld">Due date <span className="text-gray-700">:10/02/2023</span></div>
+                        </div>
+                        <div className='flex'>
+                            <div className="w-1/2 font-semibld">Status <span className="text-gray-700"> :Completed</span></div>
+                        </div>
                     </div>
-                    <div className="font-semibld">
-                        Description: <span className="text-gray-700">
-                            n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final co
-                        </span>
+                    <div className="flex justify-start items-center">
+                        <div className="font-semibld">Assigned to</div>
+                        <div className="flex justify-start items-center"> <span className="text-gray-700">: Marketing Department </span> <PlanInfoViewUsers /> </div>
+                    </div>
+                    <div>
+                        <span className="font-semibold">Description </span>  <span className="text-gray-700">: n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final c</span>
                     </div>
                 </div>
 
