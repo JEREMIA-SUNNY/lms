5f8271cc1a151d77a73d31631ed3d265a5a065df
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-02-15 19:49:26 +0530

    text size changes

--- Layout/learning/course/coursedetail.js
+++ Layout/learning/course/coursedetail.js
@@ -13,7 +13,7 @@ const CourseDetailLayout = () => {
     return (
         <>
             <CourseAction />
-            <section className="my-4 w-full bg-white p-4 shadow rounded-lg border border-[#419a00]">
+            <section className="my-4 w-full text-sm bg-white p-4 shadow rounded-lg border border-[#419a00]">
                 <DetailHeader course={course} id={id} />
                 {course?.price === "0" ? [1, 2, 3, 4, 5, 6].map((item) => {
                     return (
