f38947ef7a60e1f92e8d1c6ae88413fd66b474b2
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-02-21 13:07:55 +0530

    modified candidate cards

--- Layout/recruitment/candidates/index.js
+++ Layout/recruitment/candidates/index.js
@@ -5,6 +5,9 @@ const CandidatesLayout = () => {
     return (
         <div className="mt-4 grid gap-4 grid-cols-3">
             {appliedUser?.map((item, i) => {
+                item['source'] = 'Indeed'
+                item['vendor'] = 'Ame COrp'
+                item['landingPage'] = true
                 return (
                     <CandidateModalInfo key={i} item={item} />
                 )
