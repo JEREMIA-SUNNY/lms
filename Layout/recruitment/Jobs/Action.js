6d05d1850b109e07dce57a2dfc26891eea86a440
Naresh Kumar Gautam <nnaresh0101@gmail.com>
2023-02-23 21:50:53 +0530

    updated ui of candidate card

--- Layout/recruitment/Jobs/Action.js
+++ Layout/recruitment/Jobs/Action.js
@@ -4,7 +4,6 @@ import EditJobBtn from '../components/jobs/editJobBtn'
 import { useState } from 'react'
 import DeleteAlert from 'components/DeleteAlert'
 import { useRouter } from 'next/router'
-import CandidatesFilter from 'components/filter/recruitment/CandidatesFilter'
 
 const tablist = [
     { title: 'Job Details' },
@@ -72,7 +71,6 @@ const JobAction = ({ selectedTab, setSelectedTab }) => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
                     </svg>
                 </IconButton>
-                <CandidatesFilter />
             </div>
         </div>
     )
